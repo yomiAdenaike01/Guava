bd2587a4c17873c89352bd4ec492fcea9e6895db
Yomi Adenaike <adenaikeyomi@gmail.com>
2019-07-30 08:21:35 +0100

    Changed the download method. Cannot download from oracle website.

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const cp = require("child_process");
 const remote = require("electron").remote;
+const {shell,dialog} = require("electron").remote;
 //Checks for java installation
-cp.exec("java -v",(err,stdout,stderr)=>{
+cp.exec("javac -version",(err,stdout,stderr)=>{
     if(err){
       InitJavaDownloadAndInstallation(err);
     }else if(stderr){
@@ -18,43 +19,34 @@ cp.exec("java -v",(err,stdout,stderr)=>{
  */
 const InitJavaDownloadAndInstallation = (err) => {
   //Display the notification first 
-  var notifyJavaNotInstalled = new Notification("JAVA not installed","We have found that java is not installed on this machine, would you like to download it ?");
-  CreateButton(notifyJavaNotInstalled,"Download Java ?",DownloadJava)
-}
-
-/**
- * Init the electron downloader to install java 
- * Create a new notification to display that the latest java JDK is being donwloaded
- * Create a new notification once it has been completed.
- *  Request if they want to install
- * */ 
- function DownloadJava() {
-  //Display notification of download 
-  new Notification("Download","Currently downloading the latest java version...");
-  //init the download
-  remote.getCurrentWebContents().downloadURL("https://www.google.com/search?q=image&tbm=isch&source=iu&ictx=1&fir=QJ7Sqsi_rIj3AM%253A%252CGqyXoQNMf1XwFM%252C_&vet=1&usg=AI4_-kTbcVAM2zKBISEuO0GJQnw5nvB5dg&sa=X&ved=2ahUKEwjp9pa2qtvjAhWLZMAKHXCfC3YQ9QEwAHoECAMQMg#imgrc=QJ7Sqsi_rIj3AM:");
-
-  //Check the download progress
-  remote.getCurrentWebContents().on("will-download",()=>{
-    
-  })
+  var notifyJavaNotInstalled = new Notification("Java compiler not installed","We have found that java is not installed on this machine, would you like to download it ?");
+  CreateNotificationWithButton(notifyJavaNotInstalled,"Go To Download Java",DownloadJava)
 }
 /**
- * Requres the path that java was downloaded to 
- * Once the path has been found run child exec on the file to install it 
- * Create notification at the end of the installation once it has been completed
- * Urge them to restart the editor once the installation is done.
- * @param {*} pathToInstallJava 
+ * Can't start the download because they have to verify the liscence agreement
+ * 
+ */ 
+ const DownloadJava =()=> {
+   new Notification("To Note!","Remeber the installation path of Java, you will need it later.")
+   shell.openExternal("https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html");
+}
+/**
+ * Get the request the java installation path 
+ * Set the environmental variable from there
  */
- function InstallJava(pathToInstallJava){
+const SetJavaEnvironmentVariable = () =>{
+  dialog.showOpenDialog(remote.getCurrentWindow(),null,(file)=>{
+    if(file !== undefined){
 
+    }
+  });
 }
 
 /**
  * Creates a button element and appends it to the notification body 
  * @param {} text 
  */
-const CreateButton = (notification,text,methodToRun) =>{
+const CreateNotificationWithButton = (notification,text,methodToRun) =>{
   notification;
   if(notification){
 
@@ -73,3 +65,37 @@ const CreateButton = (notification,text,methodToRun) =>{
     notificationBody.appendChild(addButton);
   }
 }
+
+// /**
+//  * Find out whether the users machine is 64 bit or 32 bit
+//  * Match the users numbers
+//  */
+// const getUserMachineArchitecture = () =>{
+//   //Check OS 
+//   if(process.platform === "win32"){
+//   cp.exec("wmic os get osarchitecture",(err,stdout,stderr)=>{
+//     if(err){
+
+//     }else if(stderr){
+
+//     }else{
+//       var match = /64/.test(stdout);
+//       if(match === true){
+//         return "windows-64-bit";
+//       }
+//     }
+//   });
+// }else if(process.platform === "linux"){
+//   cp.exec("wmic os get osarchitecture",(err,stdout,stderr)=>{
+//     if(err){
+//       new Notification("Error!","Encountered error when checking the systems architecture.")
+//     }else if(stderr){
+
+//     }else{
+//       if(stdout.match("")){
+
+//       }
+//     }
+//   });
+// }
+// }
