1e7c5f20cab56a99ae650642d4aa7b98d9961931
Yomi Adenaike <adenaikeyomi@gmail.com>
2019-08-02 03:03:30 +0100

    Added main methods and notifications, cannot run with elevated privilages

--- main.js
+++ main.js
@@ -3,7 +3,43 @@ const remote = require("electron").remote;
 const {shell,dialog,app} = require("electron").remote;
 const fs = require("fs");
 const path = require("path");
-const progressBar = require(path.join(__dirname,"./progressBar"));
+const ProgressBar = require(path.join(__dirname,"./ProgressBar"));
+
+/**
+ * Runs the method after 1 second to not overwhelm the user
+ */
+setTimeout(function(){
+  init();
+},2000);
+
+
+  //Create menu item for java 
+const myPluginDropMenu = new dropMenu({
+  id:"java_set_up"
+});
+myPluginDropMenu.setList({
+  "button": "Java",
+  "list":{
+    "Check for java compiler installation":{
+      click:function(){
+       init();
+      }
+    }
+  }
+})
+/**
+ * Check local storage for the download path 
+ * If there is no local storage then run the run java check and then begin the process
+ */
+function init(){
+  if(process.platform === "win32" && localStorage.getItem("save_path") === null){
+    RunJavaCheck();
+  }else if(localStorage.getItem("save_path") !== null){
+    InstallJavaController("installation_found",localStorage.getItem("save_path"));
+  }
+}
+
+
 /**
  * Check for java
  */
@@ -19,11 +55,7 @@ function RunJavaCheck(){
   });
 }
 
-if(process.platform === "win32" && localStorage.getItem("save_path") === null){
-  RunJavaCheck();
-}else if(localStorage.getItem("save_path") !== null){
-  InstallJavaController("installation_found",localStorage.getItem("save_path"));
-}
+
   /**
  * 
  * @param {*} err
@@ -45,36 +77,30 @@ const InitJavaDownloadAndInstallation = (err) => {
  * 
  * TEST LINK = https://www.google.com/search?q=image&tbm=isch&source=iu&ictx=1&fir=QJ7Sqsi_rIj3AM%253A%252CGqyXoQNMf1XwFM%252C_&vet=1&usg=AI4_-kTbcVAM2zKBISEuO0GJQnw5nvB5dg&sa=X&ved=2ahUKEwjv__3Xx93jAhVRa8AKHX7GBQ4Q9QEwAHoECAcQMg#imgrc=QJ7Sqsi_rIj3AM:
  */ 
- const DownloadJava =()=> {
+function DownloadJava() {
 
     let downloadProgress;
-    progressBar.CreateProgressBar();
+    ProgressBar.CreateProgressBar();
 
     new Notification("JDK Progress","Check the console to view the download progress, We will notify you when the download is complete.");
     let userDownloadPath = app.getPath("downloads").toString();
-
     let downloadURL = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=238713_478a62b7d4e34b78b671c754eaaf38ab" ;
-
-    
     remote.getCurrentWebContents().downloadURL(downloadURL);
     remote.getCurrentWebContents().session.on("will-download",(event,item,webContents)=>{  
-
       item.setSavePath(userDownloadPath);
       downloadFilePath = item.getSavePath()+'\\'+item.getFilename();
-
       item.on("updated",(event,state)=>{
 
         if(state === "interrupted"){
           new Notification("Download Interrupted", "The Java JDK download has been interrupted, woudld you like to restart the download ?",{"Yes":function()
         {
-          progressBar.RemoveProgressBar();
-
+          ProgressBar.RemoveProgressBar();
         }})
         }
 
         if(state === "progressing"){
           downloadProgress = item.getReceivedBytes() * 100/ item.getTotalBytes();
-          progressBar.UpdateProgressBar(downloadProgress);
+          ProgressBar.UpdateProgressBar(downloadProgress);
         }
       });
 
@@ -100,11 +126,10 @@ const InitJavaDownloadAndInstallation = (err) => {
  */
 function InstallJavaController(state,savePath) {
   if(state !== "installation_found"){
-  progressBar.RemoveProgressBar();
+    ProgressBar.RemoveProgressBar();
     //Write installation path to JSON file
   }else{
     new Notification("JDK Download File Found !","We found a prior downloaded JDK file, would you like to install it or re-download a new version ?",
-    
     {"Install":function(){
 
       RunInstallCommand();
@@ -113,9 +138,11 @@ function InstallJavaController(state,savePath) {
     
       new Notification("Set Environment Variable","Have you set the environment variable for the compiler ?",{
         "No, can you set it for me ?":function(){
-
+          OpenInstallationDirectory();
         },"Yes, I have.":function(){
+
           RunJavaCheck()
+
         }
       });
     }
@@ -126,19 +153,20 @@ function InstallJavaController(state,savePath) {
 
 /**
  * 
- * Hoisting
  * Runs the installation command 
  */
 function RunInstallCommand(){
   var savePath = localStorage.getItem("save_path");
-cp.execFile(savePath,(err,data)=>{
-    if(err){
-      InstallationError(err)    
-    }else{
-      new Notification("Installing Java","Running the java installation, once complete follow the following instructions")
-    }
-  });
+  cp.execFile(savePath,(err,data)=>{
+      if(err){
+        InstallationError(err)    
+      }else{
+        new Notification("Installing Java","Running the java installation, once complete follow the following instructions")
+      }
+    });
 }
+
+
 /**
  * Display notification for opening the containing folder or open the dev tools to inspect the error.
  * @param {*} err 
@@ -156,14 +184,55 @@ function InstallationError(err){
 }
 
 /**
- * Get the request the java installation path 
- * Set the environmental variable from there
+ * Get the installation path of the JDK 
+ * And then run the command in the terminal
  */
-function SetJavaEnvironmentVariable(){
-  dialog.showOpenDialog(remote.getCurrentWindow(),null,(file)=>{
-    if(file !== undefined){
-      
+function OpenInstallationDirectory(){ 
+  const dialogProperties = {
+    properties:["openDirectory"]
+  };
+  dialog.showOpenDialog(remote.getCurrentWindow(),dialogProperties,(dir)=>{
+    if(dir != undefined || dir != null){
+      alert(dir);
+      //location the installation and save it to localstorage
+      localStorage.setItem("jdk_installation_path",dir);
+      SetJavaEnvironmentVariableCommand();
+    }else{
+      new Notification("Error!","You didn't select the java installation path, please select a path",{
+        "Select Installation Folder":OpenInstallationDirectory
+      });
     }
   });
 }
+/**
+ * Runs the command 
+ * On error will ask to run it again 
+ * If the error doesnt work then direct them to the website
+ */
+function SetJavaEnvironmentVariableCommand(){
+  var installationPath = localStorage.getItem("jdk_installation_path");
+  cp.exec(`setx -m JAVA_HOME "${installationPath}"`,function(err,stdout,stderr){
+    if(err){
+      console.log(localStorage.getItem("jdk_installation_path"));
+      console.log(err);
+      //Opens the notification to the guide
+      new Notification("Error setting variable", "Error found when setting the environmental variable, please find the guide to setting the variable below.",{"Read Guide":function(){
+        //Open the installation guide
+        shell.openExternal("https://confluence.atlassian.com/conf60/setting-the-java_home-variable-in-windows-852732596.html")
+      },"No":function(){
+        //Close the notification
+      }
+    });
+    }else{
+      
+      console.log("STDERR",stderr);
+      console.log("STDOUT",stdout);
 
+      new Notification("Installation Complete 👍","The Java JDK installation is complete, you will need to restart the editor to see whether it has installed correctly.",{"Restart Now":function(){
+        // Restart the application
+      },"Restart Later":function(){
+        //Close the notification
+      }})
+    }
+  });
+}
